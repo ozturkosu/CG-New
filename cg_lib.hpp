eeb41b0a041a0fa8f5ac64dfc4b9e2432f72fe22
STORM RAIN 71 <STORM71@STORMs-MacBook-Pro.local>
2017-12-21 00:21:29 -0500

    second commit

---
+++ cg_lib.hpp
@@ -0,0 +1,182 @@
+# include <queue>
+#include <string>
+using namespace std;
+
+int i4_min ( int i1, int i2 );
+double *orth_random ( int n, int &seed );
+double *pds_random ( int n, int &seed );
+double r8_normal_01 ( int &seed );
+double r8_sign ( double x );
+double r8_uniform_01 ( int &seed );
+
+void r83_cg ( int n, double a[], double b[], double x[] );
+double *r83_dif2 ( int m, int n );
+double *r83_mv ( int m, int n, double a[], double x[] );
+double *r83_res ( int m, int n, double a[], double x[], double b[] );
+
+void r83s_cg ( int n, double a[], double b[], double x[] );
+double *r83s_dif2 ( int m, int n );
+double *r83s_mv ( int m, int n, double a[], double x[] );
+double *r83s_res ( int m, int n, double a[], double x[], double b[] );
+
+void r83t_cg ( int n, double a[], double b[], double x[] );
+double *r83t_dif2 ( int m, int n );
+double *r83t_mv ( int m, int n, double a[], double x[] );
+double *r83t_res ( int m, int n, double a[], double x[], double b[] );
+
+void r8ge_cg ( int n, double a[], double b[], double x[] );
+double *r8ge_dif2 ( int m, int n );
+double *r8ge_mv ( int m, int n, double a[], double x[] );
+double *r8ge_res ( int m, int n, double a[], double x[], double b[] );
+
+void r8mat_copy ( int m, int n, double a1[], double a2[] );
+void r8mat_house_axh ( int n, double a[], double v[] );
+double *r8mat_identity_new ( int n );
+void r8mat_print ( int m, int n, double a[], string title );
+void r8mat_print_some ( int m, int n, double a[], int ilo, int jlo, int ihi,
+  int jhi, string title );
+double *r8mat_zero_new ( int m, int n );
+
+void r8pbu_cg ( int n, int mu, double a[], double b[], double x[] );
+double *r8pbu_dif2 ( int m, int n, int mu );
+double *r8pbu_mv ( int m, int n, int mu, double a[], double x[] );
+double *r8pbu_res ( int m, int n, int mu, double a[], double x[], double b[] );
+
+void r8sd_cg ( int n, int ndiag, int offset[], double a[], double b[], 
+  double x[] );
+double *r8sd_dif2 ( int m, int n, int ndiag, int offset[] );
+double *r8sd_mv ( int m, int n, int ndiag, int offset[], double a[], double x[] );
+double *r8sd_res ( int m, int n, int ndiag, int offset[], double a[], 
+  double x[], double b[] );
+
+void r8sp_cg ( int n, int nz_num, int row[], int col[], double a[], 
+  double b[], double x[] );
+double *r8sp_dif2 ( int m, int n, int nz_num, int row[], int col[] );
+double *r8sp_mv ( int m, int n, int nz_num, int row[], int col[], 
+  double a[], double x[] );
+double *r8sp_res ( int m, int n, int nz_num, int row[], int col[], double a[], 
+  double x[], double b[] );
+
+void r8vec_copy ( int n, double a1[], double a2[] );
+double r8vec_dot_product ( int n, double a1[], double a2[] );
+double *r8vec_house_column ( int n, double a[], int k );
+double r8vec_norm ( int n, double a[] );
+double r8vec_norm_affine ( int n, double a[], double b[] );
+void r8vec_print ( int n, double a[], string title );
+double *r8vec_uniform_01_new ( int n, int &seed );
+double *r8vec_zero_new ( int n );
+
+void timestamp ( );
+
+///////
+
+bool improveMethodFOrCheckSUm ( int n, double a[] ,  double x[] , double b[] , double curFunction);
+//New Function which was added by Emin
+void r8ge_cg_emin ( int n, double a[], double b[], double x[] , int range1 , int range2 , int k) ;//, double r[]);
+void r8ge_cg_newIndicatior_emin(int n , double a[] , double b[] , double x[] , int range1 , int range2 , int k) ; //, double r[]) ;
+void r8ge_cg_newImprovement_emin(int n , double a[] , double b[] , double x[] ,int range1, int range2 , int k ) ;
+void r8ge_cg_checkSum_emin(int n , double a[] , double b[] , double x[] , int range1, int range2, int k )  ;
+void r8ge_cg_newAlphaImprovement_emin(int n , double a[] ,double b[] , double x2[] , int range1 ,int  range2 ,int k) ;
+bool r8ge_cg_newAlphaImprovement_eminForRelativeError(int n , double a[] ,double b[] , double x[] , int range1 ,int  range2 ,int k) ;
+void r8ge_cg_checkSum_emin2(int n ,  double a[] , double  b[] , double  x2[] , int  range1, int  range2, int k ) ;
+
+void getCurIndicatorValue( double x[], double r[] , double b[]) ;
+void initialCheckSum(int n , double r[] , double p[] , double x[]);
+bool checkSumMethod(int n , double r[] , double p[] , double x[]) ;
+bool checkSumImpMethod( int n , double a[] ,double x[] , double b[]) ;
+
+//*********************
+
+
+
+double getResidual ( int m, int n, double a[], double x[], double b[] );
+double getFunctionIndicatorCunverge ( int n, double x[] , double b[] ) ;
+void injectBitflip ( int n, double a[], double x[], double b[] );
+void injectBitFlipNotRandom( int n , double a[] , double x[] , double b[] , int k , int range1 , int range2);
+void init ( int iter, int winSize, double thres, int flipPosition);
+void finish ();
+bool isFlipped ( double curRes );
+bool isFlippedIndicator (double curRes) ;
+bool isDetected( double curFunction ) ;//, queue<double> & detector) ;
+void recordResidual ( double curRes );
+void recordIndicator(double curFunction) ;
+void getIndicator( double curFunction ) ;//, queue<double> & detector) ;
+bool improveMethodWithAlpha(double curFunct, double alpha);
+bool improveMethod(double curFunct) ;
+
+
+#ifndef GLOBAL
+#define GLOBAL
+class Global {
+ public:
+  static int MAX_ITER;
+  static int windowSize;
+  static queue<double> window;
+  static queue<double> windowForIndicator;
+  static double threshold;
+  static int pos;
+  static int successfulRate;
+  static int undetectedNumber;
+  static int ImproveRate ;
+  static int CheckSumImproveRate ;
+  static double gTrend;
+  static double funGTrend;
+  static double convergeValue;
+  static double previousValueOfIndicator ;
+  static double globalTotalDifference ;
+  static double checkSumForX;
+  static double checkSumForP;
+  static double checkSumForR;
+
+  static queue<double> diffs;
+  static queue<double> diffsImprove ;
+  static queue<double> trend;
+  static queue<double> funcIndicatorTrend;
+  static queue<double> funcIndicatorDiffs;
+  static queue<double> eminDetector;
+};
+
+
+// in case of the compiler error - undefined references, please uncomment this
+int Global::MAX_ITER = 0;
+int Global::windowSize = 0;
+int Global::successfulRate=0;
+int Global::undetectedNumber=0;
+int Global::ImproveRate = 0;
+int Global::CheckSumImproveRate = 0;
+queue<double> Global::window;
+queue<double> Global::windowForIndicator;
+double Global::threshold = 0;
+int Global::pos = 0;
+double Global::gTrend = 0;
+double Global::funGTrend=0;
+double Global::convergeValue=0;
+double Global::previousValueOfIndicator = 0;
+double Global::globalTotalDifference =0 ;
+double Global::checkSumForX = 0 ;
+double Global::checkSumForP = 0 ;
+double Global::checkSumForR = 0 ;
+
+queue<double> Global::diffs;
+queue<double> Global::diffsImprove;
+queue<double> Global::trend;
+queue<double> Global::funcIndicatorTrend;
+queue<double> Global::funcIndicatorDiffs;
+queue<double> Global::eminDetector;
+
+
+
+
+
+/*
+int Global::MAX_ITER = 0;
+int Global::windowSize = 0;
+queue<double> Global::window;
+double Global::threshold = 0;
+int Global::pos = 0;
+double Global::gTrend = 0;
+queue<double> Global::diffs;
+queue<double> Global::trend;
+*/
+#endif
+
---
+++ main.cpp
@@ -0,0 +1,760 @@
+# include <cmath>
+# include <cstdlib>
+# include <cstring>
+# include <iomanip>
+# include <iostream>
+# include <time.h>
+# include "omp.h"
+
+using namespace std;
+
+# include "cg_lib.hpp"
+
+int main (int argc, char** argv);
+void r8ge_cg_start (int windowSize, int psize, double threshold, int fPos ,int range1, int range2 , int k);
+void r8ge_cg_start_withResidual (int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k);
+void r8ge_cg_start_withImprovement (int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k);
+void r8ge_cg_start_withCheckSum (int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k) ;
+void r8ge_cg_start_withAlphaDetection (int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k) ;
+double r8ge_cg_start_withAlphaDetectionAndRelativeErrors(int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k) ;
+
+
+int main (int argc, char** argv)
+{
+  if (argc != 7) {
+    cout << "usage: ./main [windowSize] [problemSize] [flipPosition] [range1] [range2] [Bit Injecting Type]" << endl;
+    return 0;
+  }
+
+  int windowSize = atoi(argv[1]); // size of window to monitor
+  int psize = atoi(argv[2]);  // problem size
+  int fPos = atoi(argv[3]);   // iteration to flip bits
+  int range1=atoi(argv[4]);   //   Bit flip range start
+  int range2=atoi(argv[5]) ;  // Bit flip range finish
+  int k=atoi(argv[6]) ;  // K for injecting type
+  // threshold by default set to 0.1, please modify as needed. 
+  double threshold = 1e-1;
+  
+  //int& succesfulrate= Global::successfulRate ;
+
+  // iteration to flip bits
+  
+  timestamp ( );
+  cout << "\n";
+  cout << "CG_PRB\n";
+  cout << "  C++ version\n";
+  cout << "  Test the CG library.\n";
+
+
+  //queue<double>& detector = Global::eminDetector;
+
+  //This part of only fn 
+
+  /*
+
+  for (int i = 0; i < 100; ++i)
+  {
+   
+    Global::previousValueOfIndicator =0 ;
+
+    cout << " Number of Iteration = " << i << endl ;
+
+    r8ge_cg_start (windowSize, psize, threshold, fPos , range1 , range2 , k );
+
+    cout << "  successfulRate = " << Global::successfulRate << " % " <<endl ;
+
+    //detector.pop() ;
+    //detector.pop() ;
+  }
+
+  */
+
+  int succesfulrateForIndicator = Global::successfulRate ;
+  Global::successfulRate = 0 ;
+
+  /*
+  for (int i = 0; i < 100; ++i)
+  {
+   
+    Global::previousValueOfIndicator =0 ;
+
+    cout << " Number of Iteration = " << i << endl ;
+
+    r8ge_cg_start_withResidual (windowSize, psize, threshold, fPos , range1 , range2 , k );
+
+    cout << "  successfulRate = " << Global::successfulRate << " % " <<endl ;
+
+    //detector.pop() ;
+    //detector.pop() ;
+  }
+
+  int succesfulrateForResidual = Global::successfulRate ;
+  
+  */
+
+
+  // Fn with improvement method 
+
+  
+
+
+  cout << " \n";
+  cout << "******************************** \n";
+  cout << "******************************** \n";
+  cout << "******For improvement ********** \n";
+
+  queue<double>& diffsImp = Global::diffsImprove;
+
+
+  double& totalDifference =  Global::globalTotalDifference;
+  double& prevValueOfIndicator = Global::previousValueOfIndicator;
+  //int& ImpRate= Global::ImproveRate ;
+
+  double averageRelativeError = 0 ;
+
+  double res =0;
+
+  for (int i = 0; i < 100; ++i)
+  {
+    
+    prevValueOfIndicator=0 ;
+    totalDifference=0 ;
+    //ImpRate=0;
+   
+
+    cout << "Number of Experience = " << i << endl ;
+
+    //r8ge_cg_start_withImprovement(windowSize, psize, threshold, fPos , range1 , range2 , k );
+    //r8ge_cg_start_withAlphaDetection(windowSize, psize, threshold, fPos , range1 , range2 , k );
+
+    res = r8ge_cg_start_withAlphaDetectionAndRelativeErrors(windowSize, psize, threshold, fPos , range1 , range2 , k ) ;
+
+    cout<<" Res = "<<res<<endl ;
+    averageRelativeError = averageRelativeError + res ;
+
+
+
+
+    cout << "SuccessfulRate = " << Global::successfulRate << " % " <<endl ;
+    cout << "SuccessfulRate for improve = " << Global::ImproveRate << " % " <<endl ;
+
+
+
+    
+    
+
+    while (!diffsImp.empty())
+    {
+     //sum += myqueue.front();
+        diffsImp.pop();
+    }
+
+  }
+
+  int succesfulrateForImprovement = Global::successfulRate ;
+  double unsuccesful = Global::undetectedNumber ;
+
+  averageRelativeError = averageRelativeError / unsuccesful ;
+
+  
+
+
+
+  
+    
+//
+//  Terminate.
+//
+  cout << " \n";
+  cout << "CG_PRB\n";
+  cout << "  Normal end of execution.\n";
+  //cout << "  successfulRate for fn indicator"<<succesfulrateForIndicator<<  endl;
+  cout << "  SuccessfulRate for fn with improvement "<< succesfulrateForImprovement <<endl ;
+  cout << "  Averae Relative Error "<<averageRelativeError<<endl ;
+  cout << " \n";
+
+
+  
+  timestamp ( );
+
+  return 0;
+
+
+
+
+}
+
+
+
+//****************************************************************************80
+
+void r8ge_cg_start (int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k)
+
+
+{
+  double *a;
+  double *b;
+  double e_norm;
+  int i;
+  int n;
+  double *r;
+  double r_norm;
+  int seed;
+  double *x1;
+  double *x2;
+
+  int winSize = windowSize;
+  double thres = threshold;
+  int flipPosition = fPos;
+  
+  cout << "\n";
+  cout << "R8GE_CG_TEST\n";
+  cout << "  R8GE_CG applies CG to a full storage matrix.\n";
+
+  //cout << " Number of "
+
+
+
+  n = psize;
+//
+//  Let A be the -1 2 -1 matrix.
+//
+  srand (time(NULL));
+  seed = rand();
+  a = pds_random ( n, seed );
+//
+//  Choose a random solution.
+//
+  x1 = r8vec_uniform_01_new ( n, seed );
+//
+//  Compute the corresponding right hand side.
+//
+  b = r8ge_mv ( n, n, a, x1 );
+//
+//  Call the CG routine.
+//
+  x2 = new double[n];
+  for ( i = 0; i < n; i++ )
+  {
+    x2[i] = 1.0;
+  }
+  init (n, winSize, thres, flipPosition);
+  //r8ge_cg ( n, a, b, x2 );
+
+  //r8ge_cg_emin( n ,a , b , x2) ; 
+  r8ge_cg_newIndicatior_emin(n , a , b , x2 , range1 , range2 , k) ;
+
+
+//
+//  Compute the residual.  //Emin Note = calculation residual again is non necessary because cg calculate residual as well 
+//
+  //r = r8ge_res ( n, n, a, x2, b );
+  //r_norm = r8vec_norm ( n, r );
+//
+//  Compute the error.
+//
+  //e_norm = r8vec_norm_affine ( n, x1, x2 );
+//
+//  Report.
+//
+  cout << "\n";
+  //cout << "  Number of variables N = " << n << "\n";
+  //cout << "  Norm of residual ||Ax-b|| = " << r_norm << "\n";
+  //cout << "  Norm of error ||x1-x2|| = " << e_norm << "\n";
+//
+//  Free memory.
+//
+  delete [] a;
+  delete [] b;
+  delete [] r;
+  delete [] x1;
+  delete [] x2;
+
+  
+  finish();
+  return;
+}
+//****************************************************************************80
+
+void r8ge_cg_start_withResidual (int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k)
+
+
+{
+  double *a;
+  double *b;
+  double e_norm;
+  int i;
+  int n;
+  double *r;
+  double r_norm;
+  int seed;
+  double *x1;
+  double *x2;
+
+  int winSize = windowSize;
+  double thres = threshold;
+  int flipPosition = fPos;
+  
+  cout << "\n";
+  cout << "R8GE_CG_TEST\n";
+  cout << "  R8GE_CG applies CG to a full storage matrix.\n";
+
+  //cout << " Number of "
+
+
+
+  n = psize;
+//
+//  Let A be the -1 2 -1 matrix.
+//
+  srand (time(NULL));
+  seed = rand();
+  a = pds_random ( n, seed );
+//
+//  Choose a random solution.
+//
+  x1 = r8vec_uniform_01_new ( n, seed );
+//
+//  Compute the corresponding right hand side.
+//
+  b = r8ge_mv ( n, n, a, x1 );
+//
+//  Call the CG routine.
+//
+  x2 = new double[n];
+  for ( i = 0; i < n; i++ )
+  {
+    x2[i] = 1.0;
+  }
+  init (n, winSize, thres, flipPosition);
+  //r8ge_cg ( n, a, b, x2 );
+
+  r8ge_cg_emin( n ,a , b , x2 , range1 , range2 , k) ; 
+  //r8ge_cg_newIndicatior_emin(n , a , b , x2 , range1 , range2 , k) ;
+
+
+//
+//  Compute the residual.  //Emin Note = calculation residual again is non necessary because cg calculate residual as well 
+//
+  //r = r8ge_res ( n, n, a, x2, b );
+  //r_norm = r8vec_norm ( n, r );
+//
+//  Compute the error.
+//
+  //e_norm = r8vec_norm_affine ( n, x1, x2 );
+//
+//  Report.
+//
+  cout << "\n";
+  //cout << "  Number of variables N = " << n << "\n";
+  //cout << "  Norm of residual ||Ax-b|| = " << r_norm << "\n";
+  //cout << "  Norm of error ||x1-x2|| = " << e_norm << "\n";
+//
+//  Free memory.
+//
+  delete [] a;
+  delete [] b;
+  delete [] r;
+  delete [] x1;
+  delete [] x2;
+
+  
+  finish();
+  return;
+}
+//****************************************************************************80
+
+void r8ge_cg_start_withImprovement (int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k)
+{
+
+  double *a;
+  double *b;
+
+  double e_norm;
+  int i;
+  int n;
+  double *r;
+
+  double r_norm;
+  int seed;
+
+  double *x1;
+  double *x2;
+
+  int winSize = windowSize;
+  double thres = threshold;
+  int flipPosition = fPos;
+  
+  cout << "\n";
+  cout << "R8GE_CG_TEST\n";
+  cout << "  R8GE_CG applies CG to a full storage matrix.\n";
+
+  //cout << " Number of "
+
+
+
+  n = psize;
+//
+//  Let A be the -1 2 -1 matrix.
+//
+  srand (time(NULL));
+  seed = rand();
+  a = pds_random ( n, seed );
+//
+//  Choose a random solution.
+//
+  x1 = r8vec_uniform_01_new ( n, seed );
+//
+//  Compute the corresponding right hand side.
+//
+  b = r8ge_mv ( n, n, a, x1 );
+//
+//  Call the CG routine.
+//
+  x2 = new double[n];
+  for ( i = 0; i < n; i++ )
+  {
+    x2[i] = 1.0;
+  }
+  init (n, winSize, thres, flipPosition);
+  //r8ge_cg ( n, a, b, x2 );
+
+  //r8ge_cg_emin( n ,a , b , x2 , range1 , range2 , k) ; 
+  //r8ge_cg_newIndicatior_emin(n , a , b , x2 , range1 , range2 , k) ;
+  r8ge_cg_newImprovement_emin(n , a , b , x2 , range1 , range2 , k) ;
+
+//
+//  Compute the residual.  //Emin Note = calculation residual again is non necessary because cg calculate residual as well 
+//
+  //r = r8ge_res ( n, n, a, x2, b );
+  //r_norm = r8vec_norm ( n, r );
+//
+//  Compute the error.
+//
+  //e_norm = r8vec_norm_affine ( n, x1, x2 );
+//
+//  Report.
+//
+  cout << "\n";
+  //cout << "  Number of variables N = " << n << "\n";
+  //cout << "  Norm of residual ||Ax-b|| = " << r_norm << "\n";
+  //cout << "  Norm of error ||x1-x2|| = " << e_norm << "\n";
+//
+//  Free memory.
+//
+  delete [] a;
+  delete [] b;
+  delete [] r;
+  delete [] x1;
+  delete [] x2;
+
+  
+  finish();
+  return;
+
+
+}
+
+
+void r8ge_cg_start_withCheckSum (int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k)
+{
+
+  double *a;
+  double *b;
+
+  double e_norm;
+  int i;
+  int n;
+  double *r;
+
+  double r_norm;
+  int seed;
+
+  double *x1;
+  double *x2;
+
+  int winSize = windowSize;
+  double thres = threshold;
+  int flipPosition = fPos;
+  
+  cout << "\n";
+  cout << "R8GE_CG_TEST\n";
+  cout << "  R8GE_CG applies CG to a full storage matrix.\n";
+
+  //cout << " Number of "
+
+
+
+  n = psize;
+//
+//  Let A be the -1 2 -1 matrix.
+//
+  srand (time(NULL));
+  seed = rand();
+  a = pds_random ( n, seed );
+//
+//  Choose a random solution.
+//
+  x1 = r8vec_uniform_01_new ( n, seed );
+//
+//  Compute the corresponding right hand side.
+//
+  b = r8ge_mv ( n, n, a, x1 );
+//
+//  Call the CG routine.
+//
+  x2 = new double[n];
+  for ( i = 0; i < n; i++ )
+  {
+    x2[i] = 1.0;
+  }
+  init (n, winSize, thres, flipPosition);
+  //r8ge_cg ( n, a, b, x2 );
+
+  //r8ge_cg_emin( n ,a , b , x2 , range1 , range2 , k) ; 
+  //r8ge_cg_newIndicatior_emin(n , a , b , x2 , range1 , range2 , k) ;
+  //r8ge_cg_newImprovement_emin(n , a , b , x2 , range1 , range2 , k) ;
+
+  //r8ge_cg_checkSum_emin( n ,  a ,  b ,  x2 ,  range1,  range2, k ) ;
+  r8ge_cg_checkSum_emin2( n ,  a ,  b ,  x2 ,  range1,  range2, k ) ;
+
+
+//
+//  Compute the residual.  //Emin Note = calculation residual again is non necessary because cg calculate residual as well 
+//
+  //r = r8ge_res ( n, n, a, x2, b );
+  //r_norm = r8vec_norm ( n, r );
+//
+//  Compute the error.
+//
+  //e_norm = r8vec_norm_affine ( n, x1, x2 );
+//
+//  Report.
+//
+  cout << "\n";
+  //cout << "  Number of variables N = " << n << "\n";
+  //cout << "  Norm of residual ||Ax-b|| = " << r_norm << "\n";
+  //cout << "  Norm of error ||x1-x2|| = " << e_norm << "\n";
+//
+//  Free memory.
+//
+  delete [] a;
+  delete [] b;
+  delete [] r;
+  delete [] x1;
+  delete [] x2;
+
+  
+  finish();
+  return;
+
+
+
+
+}
+
+void r8ge_cg_start_withAlphaDetection (int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k) 
+{
+
+  double *a;
+  double *b;
+
+  double e_norm;
+  int i;
+  int n;
+  double *r;
+
+  double r_norm;
+  int seed;
+
+  double *x1;
+  double *x2;
+
+  int winSize = windowSize;
+  double thres = threshold;
+  int flipPosition = fPos;
+  
+  cout << "\n";
+  cout << "R8GE_CG_TEST\n";
+  cout << "  R8GE_CG applies CG to a full storage matrix.\n";
+
+  //cout << " Number of "
+
+
+
+  n = psize;
+//
+//  Let A be the -1 2 -1 matrix.
+//
+  srand (time(NULL));
+  seed = rand();
+  a = pds_random ( n, seed );
+//
+//  Choose a random solution.
+//
+  x1 = r8vec_uniform_01_new ( n, seed );
+//
+//  Compute the corresponding right hand side.
+//
+  b = r8ge_mv ( n, n, a, x1 );
+//
+//  Call the CG routine.
+//
+  x2 = new double[n];
+  for ( i = 0; i < n; i++ )
+  {
+    x2[i] = 1.0;
+  }
+  init (n, winSize, thres, flipPosition);
+  //r8ge_cg ( n, a, b, x2 );
+
+  //r8ge_cg_emin( n ,a , b , x2 , range1 , range2 , k) ; 
+  //r8ge_cg_newIndicatior_emin(n , a , b , x2 , range1 , range2 , k) ;
+  r8ge_cg_newAlphaImprovement_emin(n , a , b , x2 , range1 , range2 , k) ;
+
+//
+//  Compute the residual.  //Emin Note = calculation residual again is non necessary because cg calculate residual as well 
+//
+  //r = r8ge_res ( n, n, a, x2, b );
+  //r_norm = r8vec_norm ( n, r );
+//
+//  Compute the error.
+//
+  //e_norm = r8vec_norm_affine ( n, x1, x2 );
+//
+//  Report.
+//
+  cout << "\n";
+  //cout << "  Number of variables N = " << n << "\n";
+  //cout << "  Norm of residual ||Ax-b|| = " << r_norm << "\n";
+  //cout << "  Norm of error ||x1-x2|| = " << e_norm << "\n";
+//
+//  Free memory.
+//
+  delete [] a;
+  delete [] b;
+  delete [] r;
+  delete [] x1;
+  delete [] x2;
+
+  
+  finish();
+  return;
+
+
+}
+
+double r8ge_cg_start_withAlphaDetectionAndRelativeErrors(int windowSize, int psize, double threshold, int fPos , int range1 , int range2 , int k) 
+{
+
+  double *a;
+  double *b;
+
+  double e_norm;
+  int i;
+  int n;
+  //double *r;
+
+ // double r_norm;
+  int seed;
+
+  double *x1;
+  double *x2;
+
+  int winSize = windowSize;
+  double thres = threshold;
+  int flipPosition = fPos;
+  
+  cout << "\n";
+  cout << "R8GE_CG_TEST\n";
+  cout << "  R8GE_CG applies CG to a full storage matrix.\n";
+
+  //cout << " Number of "
+
+
+
+  n = psize;
+//
+//  Let A be the -1 2 -1 matrix.
+//
+  srand (time(NULL));
+  seed = rand();
+  a = pds_random ( n, seed );
+//
+//  Choose a random solution.
+//
+  x1 = r8vec_uniform_01_new ( n, seed );
+//
+//  Compute the corresponding right hand side.
+//
+  b = r8ge_mv ( n, n, a, x1 );
+//
+//  Call the CG routine.
+//
+  x2 = new double[n];
+  for ( i = 0; i < n; i++ )
+  {
+    x2[i] = 1.0;
+  }
+  init (n, winSize, thres, flipPosition);
+
+  //r8ge_cg ( n, a, b, x2 );
+
+  //r8ge_cg_emin( n ,a , b , x2 , range1 , range2 , k) ; 
+  //r8ge_cg_newIndicatior_emin(n , a , b , x2 , range1 , range2 , k) ;
+
+  bool isDetected;
+  isDetected=r8ge_cg_newAlphaImprovement_eminForRelativeError( n ,  a , b ,  x2 ,  range1 ,  range2 ,k) ;
+
+  //r8ge_cg_newAlphaImprovement_emin(n , a , b , x2 , range1 , range2 , k) ;
+
+
+  double XesNorm2 ;
+  XesNorm2= r8vec_norm(n , x2) ;
+
+  e_norm = r8vec_norm_affine ( n, x1, x2 );
+
+  double result;
+  result = e_norm / XesNorm2 ;
+
+  double finalResult;
+
+  finalResult = log10(result) ;
+
+  cout<<" Relative Error is "<<finalResult << endl ;
+
+
+
+
+
+//
+//  Compute the error.
+//
+  //e_norm = r8vec_norm_affine ( n, x1, x2 );
+//
+//  Report.
+//
+  cout << "\n";
+  //cout << "  Number of variables N = " << n << "\n";
+  //cout << "  Norm of residual ||Ax-b|| = " << r_norm << "\n";
+  //cout << "  Norm of error ||x1-x2|| = " << e_norm << "\n";
+//
+//  Free memory.
+//
+  delete [] a;
+  delete [] b;
+  //delete [] r;
+  delete [] x1;
+  delete [] x2;
+
+  
+  finish();
+
+  if(!isDetected )
+    return finalResult;
+  else
+    return 0 ;
+
+}
+
+
