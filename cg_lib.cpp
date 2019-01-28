08abba08af5ab4aaa5b2288f08197e487d18c71f
ozturkosu <ozturk.osu@gmail.com>
2019-01-28 01:39:57 -0500

    newIndicatorDefined

--- cg_lib.cpp
+++ cg_lib.cpp
@@ -3692,6 +3692,156 @@ void r8ge_cg_newIndicatior_emin(int n , double a[] , double b[] , double x[] , i
 }
 
 
+void r8ge_cg_Indicator_version2(int n , double a[] , double b[] , double x[] , int range1 , int range2 , int k)  
+{
+  double alpha;
+  double *ap;
+  double beta;
+  int i;
+  int it;
+  double *p;
+  double pap;
+  double pr;
+  double *r; //It takes it as parameter
+  double rap;
+  double curIndicatorValue ; 
+
+  double newIndicator;
+
+  cout << " Detection by  Indicator_version2 Method fn=-x^T*(-rn-b) to detect soft error  " <<endl ;
+  cout << "" <<endl ;
+
+  //
+  //  Initialize
+  //    AP = A * x,
+  //    R  = b - A * x,
+  //    P  = b - A * x.
+  //
+  ap = r8ge_mv( n, n, a, x );
+  r = new double[n];
+
+
+  for ( i = 0; i < n; i++ )
+  {
+    r[i] = b[i] - ap[i];
+  }
+
+  p = new double[n];
+  for ( i = 0; i < n; i++ )
+  {
+    p[i] = b[i] - ap[i];
+  }
+
+
+  //
+  //  Do the N steps of the conjugate gradient method.
+  //
+
+
+  for( it = 1; it <= n; it++ )
+  {
+      // inject bit flip - replace with custom bit injecter
+      if (it == Global::pos) {
+          //injectBitflip (n, a, x, b);
+
+            cout << " Fault Injecting is occurred here" <<endl ;
+            injectBitFlipNotRandom(n , r, p , x , range1 ,  range2 ,  k) ;
+
+      }
+
+
+      //
+      //  Compute the matrix*vector product AP=A*P.
+      //
+
+      delete [] ap;
+      ap = r8ge_mv ( n, n, a, p );
+      
+         //
+      //  Compute the dot products
+      //    PAP = P*AP,
+      //    PR  = P*R
+      //  Set
+      //    ALPHA = PR / PAP.
+
+     pap = r8vec_dot_product ( n, p, ap );
+     pr = r8vec_dot_product ( n, p, r );
+
+     if ( pap == 0.0 )
+     {
+        delete [] ap;
+        break;
+     }
+
+     alpha = pr / pap;
+
+     newIndicator = alpha * pr ;
+
+      //pr = (rk)^T(rk)
+      //pap = (pk)^T A (pk)  A is Symetric positive definite (SPD)
+
+      //
+      //  Set
+      //    X = X + ALPHA * P
+      //    R = R - ALPHA * AP.
+      //
+
+      for ( i = 0; i < n; i++ )
+      {
+        x[i] = x[i] + alpha * p[i];
+      }
+      for ( i = 0; i < n; i++ )
+      {
+        r[i] = r[i] - alpha * ap[i];
+      }
+
+      //
+      //  Compute the vector dot product
+      //    RAP = R*AP
+      //  Set
+      //    BETA = - RAP / PAP.
+      //
+      rap = r8vec_dot_product ( n, r, ap );
+
+      beta = - rap / pap;
+      //
+      //  Update the perturbation vector
+      //    P = R + BETA * P.
+      //
+      for ( i = 0; i < n; i++ )
+      {
+          p[i] = r[i] + beta * p[i];
+      }
+
+      //Error Detection part
+
+      
+      cout << " new indicator at i "<< it << " NewIndicator= " << newIndicator <<endl; 
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 
 //****************************************************************************80
 
