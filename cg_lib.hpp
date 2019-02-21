99279bb8d1d812c2275c608c00c994cec58f4f2a
ozturkosu <ozturk.osu@gmail.com>
2019-02-20 23:28:38 -0500

    before make

--- cg_lib.hpp
+++ cg_lib.hpp
@@ -137,7 +137,7 @@ class Global {
   static queue<double> funcIndicatorDiffs;
   static queue<double> eminDetector;
 };
-
+/*
 // in case of the compiler error - undefined references, please uncomment this
 int Global::MAX_ITER = 0;
 int Global::windowSize = 0;
