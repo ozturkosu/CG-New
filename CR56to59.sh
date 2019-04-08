71bc4ac2218f4fa9cf25f38728d2130ececbca49
ozturkosu <ozturk.osu@gmail.com>
2019-04-08 13:26:42 -0400

    script

---
+++ CR56to59.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+mkdir CRintoR56to59rangeNis200
+./main2 5 420 84 56 59 0 bcsstk06.mtx >> CRintoR56to59rangeNis200/inner56to63result_bcsstk06.txt
+mkdir CRintoR56to59rangeNis400
+./main2 5 420 168 56 59 0 bcsstk06.mtx >> CRintoR56to59rangeNis400/inner56to63result_bcsstk06.txt
+mkdir CRintoR56to59rangeNis600
+./main2 5 420 252 56 59 0 bcsstk06.mtx >> CRintoR56to59rangeNis600/inner56to63result_bcsstk06.txt
+mkdir CRintoR56to59rangeNis800
+./main2 5 420 336 56 59 0 bcsstk06.mtx >> CRintoR56to59rangeNis800/inner56to63result_bcsstk06.txt
+mkdir CRintoP56to59rangeNis200
+./main2 5 420 84 56 59 1 bcsstk06.mtx >> CRintoP56to59rangeNis200/inner56to63result_bcsstk06.txt
+mkdir CRintoP56to59rangeNis400
+./main2 5 420 168 56 59 1 bcsstk06.mtx >> CRintoP56to59rangeNis400/inner56to63result_bcsstk06.txt
+mkdir CRintoP56to59rangeNis600
+./main2 5 420 252 56 59 1 bcsstk06.mtx >> CRintoP56to59rangeNis600/inner56to63result_bcsstk06.txt
+mkdir CRintoP56to59rangeNis800
+./main2 5 420 336 56 59 1 bcsstk06.mtx >> CRintoP56to59rangeNis800/inner56to63result_bcsstk06.txt
+mkdir CRintoX56to59rangeNis200
+./main2 5 420 84 56 59 2 bcsstk06.mtx >> CRintoX56to59rangeNis200/inner56to63result_bcsstk06.txt
+mkdir CRintoX56to59rangeNis400
+./main2 5 420 168 56 59 2 bcsstk06.mtx >> CRintoX56to59rangeNis400/inner56to63result_bcsstk06.txt
+mkdir CRintoX56to59rangeNis600
+./main2 5 420 252 56 59 2 bcsstk06.mtx >> CRintoX56to59rangeNis600/inner56to63result_bcsstk06.txt
+mkdir CRintoX56to59rangeNis800
+./main2 5 420 336 56 59 2 bcsstk06.mtx >> CRintoX56to59rangeNis800/inner56to63result_bcsstk06.txt
