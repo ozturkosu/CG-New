312a98befdd1139d36091a5ad536d1860324b97d
ozturkosu <ozturk.osu@gmail.com>
2019-04-08 14:32:21 -0400

    script

---
+++ ProposedwithCR60to63.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+mkdir CRintoR60to63rangeNis200
+./main3 5 420 84 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis200/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoR60to63rangeNis400
+./main3 5 420 168 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis400/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoR60to63rangeNis600
+./main3 5 420 252 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis600/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoR60to63rangeNis800
+./main3 5 420 336 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis800/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoP60to63rangeNis200
+./main3 5 420 84 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis200/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoP60to63rangeNis400
+./main3 5 420 168 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis400/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoP60to63rangeNis600
+./main3 5 420 252 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis600/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoP60to63rangeNis800
+./main3 5 420 336 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis800/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoX60to63rangeNis200
+./main3 5 420 84 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis200/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoX60to63rangeNis400
+./main3 5 420 168 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis400/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoX60to63rangeNis600
+./main3 5 420 252 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis600/PIwithResidual60to63result_bcsstk06.txt
+mkdir CRintoX60to63rangeNis800
+./main3 5 420 336 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis800/PIwithResidual60to63result_bcsstk06.txt
+ 
