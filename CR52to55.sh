4400c54f81ee8d87e985e3cc3f453f751a4a3b19
ozturkosu <ozturk.osu@gmail.com>
2019-04-08 13:55:47 -0400

    script

---
+++ CR52to55.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+mkdir CRintoR52to55rangeNis200
+./main2 5 420 84 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis200/inner52to55result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis400
+./main2 5 420 168 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis400/inner52to55result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis600
+./main2 5 420 252 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis600/inner52to55result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis800
+./main2 5 420 336 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis800/inner52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis200
+./main2 5 420 84 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis200/inner52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis400
+./main2 5 420 168 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis400/inner52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis600
+./main2 5 420 252 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis600/inner52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis800
+./main2 5 420 336 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis800/inner52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis200
+./main2 5 420 84 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis200/inner52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis400
+./main2 5 420 168 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis400/inner52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis600
+./main2 5 420 252 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis600/inner52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis800
+./main2 5 420 336 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis800/inner52to55result_bcsstk06.txt
