98aea1b15289538a719b601dee2bf0a8f365754c
ozturkosu <ozturk.osu@gmail.com>
2019-04-08 14:30:36 -0400

    script

--- CR60to63.sh
+++ CR60to63.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 mkdir CRintoR60to63rangeNis200
-./main2 5 420 100 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis200/inner60to63result_bcsstk06.txt
+./main2 5 420 84 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis200/residual60to63result_bcsstk06.txt
 mkdir CRintoR60to63rangeNis400
-./main2 5 420 200 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis400/inner60to63result_bcsstk06.txt
+./main2 5 420 168 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis400/residual60to63result_bcsstk06.txt
 mkdir CRintoR60to63rangeNis600
-./main2 5 420 300 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis600/inner60to63result_bcsstk06.txt
+./main2 5 420 252 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis600/residual60to63result_bcsstk06.txt
 mkdir CRintoR60to63rangeNis800
-./main2 5 420 400 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis800/inner60to63result_bcsstk06.txt
+./main2 5 420 336 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis800/residual60to63result_bcsstk06.txt
 mkdir CRintoP60to63rangeNis200
-./main2 5 420 100 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis200/inner60to63result_bcsstk06.txt
+./main2 5 420 84 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis200/residual60to63result_bcsstk06.txt
 mkdir CRintoP60to63rangeNis400
-./main2 5 420 200 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis400/inner60to63result_bcsstk06.txt
+./main2 5 420 168 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis400/residual60to63result_bcsstk06.txt
 mkdir CRintoP60to63rangeNis600
-./main2 5 420 300 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis600/inner60to63result_bcsstk06.txt
+./main2 5 420 252 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis600/residual60to63result_bcsstk06.txt
 mkdir CRintoP60to63rangeNis800
-./main2 5 420 400 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis800/inner60to63result_bcsstk06.txt
+./main2 5 420 336 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis800/residual60to63result_bcsstk06.txt
 mkdir CRintoX60to63rangeNis200
-./main2 5 420 100 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis200/inner60to63result_bcsstk06.txt
+./main2 5 420 84 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis200/residual60to63result_bcsstk06.txt
 mkdir CRintoX60to63rangeNis400
-./main2 5 420 200 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis400/inner60to63result_bcsstk06.txt
+./main2 5 420 168 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis400/residual60to63result_bcsstk06.txt
 mkdir CRintoX60to63rangeNis600
-./main2 5 420 300 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis600/inner60to63result_bcsstk06.txt
+./main2 5 420 252 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis600/residual60to63result_bcsstk06.txt
 mkdir CRintoX60to63rangeNis800
-./main2 5 420 400 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis800/inner60to63result_bcsstk06.txt
+./main2 5 420 336 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis800/residual60to63result_bcsstk06.txt
+ 
