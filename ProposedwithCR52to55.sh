a6b18b76e1338f331b169479da8ea9db45b539f8
ozturkosu <ozturk.osu@gmail.com>
2019-04-08 14:56:49 -0400

    script

---
+++ ProposedwithCR52to55.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+mkdir CRintoR52to55rangeNis200
+./main3 5 420 84 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis200/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis400
+./main3 5 420 168 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis400/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis600
+./main3 5 420 252 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis600/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis800
+./main3 5 420 336 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis800/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis200
+./main3 5 420 84 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis200/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis400
+./main3 5 420 168 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis400/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis600
+./main3 5 420 252 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis600/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis800
+./main3 5 420 336 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis800/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis200
+./main3 5 420 84 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis200/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis400
+./main3 5 420 168 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis400/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis600
+./main3 5 420 252 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis600/PIwithResidual52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis800
+./main3 5 420 336 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis800/PIwithResidual52to55result_bcsstk06.txt
