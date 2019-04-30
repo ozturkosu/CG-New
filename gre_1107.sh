87aed1076af211436b190e0d8f632d9d3c17bc89
ozturkosu <ozturk.osu@gmail.com>
2019-04-30 02:36:11 -0400

    new script

---
+++ gre_1107.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#SBATCH --time=48:59:00
+mkdir BICGintoR60to63rangeNis200
+./mainbcg 5 1107 200 60 63 0 gre_1107.mtx >> BICGintoR60to63rangeNis200/60to63result_gre_1107.txt
+mkdir BICGintoR60to63rangeNis400
+./mainbcg 5 1107 400 60 63 0 gre_1107.mtx >> BICGintoR60to63rangeNis400/60to63result_gre_1107.txt
+mkdir BICGintoR60to63rangeNis600
+./mainbcg 5 1107 600 60 63 0 gre_1107.mtx >> BICGintoR60to63rangeNis600/60to63result_gre_1107.txt
+mkdir BICGintoR60to63rangeNis800
+./mainbcg 5 1107 800 60 63 0 gre_1107.mtx >> BICGintoR60to63rangeNis800/60to63result_gre_1107.txt
+mkdir BICGintoP60to63rangeNis200
+./mainbcg 5 1107 200 60 63 1 gre_1107.mtx >> BICGintoP60to63rangeNis200/60to63result_gre_1107.txt
+mkdir BICGintoP60to63rangeNis400
+./mainbcg 5 1107 400 60 63 1 gre_1107.mtx >> BICGintoP60to63rangeNis400/60to63result_gre_1107.txt
+mkdir BICGintoP60to63rangeNis600
+./mainbcg 5 1107 600 60 63 1 gre_1107.mtx >> BICGintoP60to63rangeNis600/60to63result_gre_1107.txt
+mkdir BICGintoP60to63rangeNis800
+./mainbcg 5 1107 800 60 63 1 gre_1107.mtx >> BICGintoP60to63rangeNis800/60to63result_gre_1107.txt
+mkdir BICGintoX60to63rangeNis200
+./mainbcg 5 1107 200 60 63 2 gre_1107.mtx >> BICGintoX60to63rangeNis200/60to63result_gre_1107.txt
+mkdir BICGintoX60to63rangeNis400
+./mainbcg 5 1107 400 60 63 2 gre_1107.mtx >> BICGintoX60to63rangeNis400/60to63result_gre_1107.txt
+mkdir BICGintoX60to63rangeNis600
+./mainbcg 5 1107 600 60 63 2 gre_1107.mtx >> BICGintoX60to63rangeNis600/60to63result_gre_1107.txt
+mkdir BICGintoX60to63rangeNis800
+./mainbcg 5 1107 800 60 63 2 gre_1107.mtx >> BICGintoX60to63rangeNis800/60to63result_gre_1107.txt
+mkdir BICGintoR56to59rangeNis200
+./mainbcg 5 1107 200 56 59 0 gre_1107.mtx >> BICGintoR56to59rangeNis200/56to59result_gre_1107.txt
+mkdir BICGintoR56to59rangeNis400
+./mainbcg 5 1107 400 56 59 0 gre_1107.mtx >> BICGintoR56to59rangeNis400/56to59result_gre_1107.txt
+mkdir BICGintoR56to59rangeNis600
+./mainbcg 5 1107 600 56 59 0 gre_1107.mtx >> BICGintoR56to59rangeNis600/56to59result_gre_1107.txt
+mkdir BICGintoR56to59rangeNis800
+./mainbcg 5 1107 800 56 59 0 gre_1107.mtx >> BICGintoR56to59rangeNis800/56to59result_gre_1107.txt
+mkdir BICGintoP56to59rangeNis200
+./mainbcg 5 1107 200 56 59 1 gre_1107.mtx >> BICGintoP56to59rangeNis200/56to59result_gre_1107.txt
+mkdir BICGintoP56to59rangeNis400
+./mainbcg 5 1107 400 56 59 1 gre_1107.mtx >> BICGintoP56to59rangeNis400/56to59result_gre_1107.txt
+mkdir BICGintoP56to59rangeNis600
+./mainbcg 5 1107 600 56 59 1 gre_1107.mtx >> BICGintoP56to59rangeNis600/56to59result_gre_1107.txt
+mkdir BICGintoP56to59rangeNis800
+./mainbcg 5 1107 800 56 59 1 gre_1107.mtx >> BICGintoP56to59rangeNis800/56to59result_gre_1107.txt
+mkdir BICGintoX56to59rangeNis200
+./mainbcg 5 1107 200 56 59 2 gre_1107.mtx >> BICGintoX56to59rangeNis200/56to59result_gre_1107.txt
+mkdir BICGintoX56to59rangeNis400
+./mainbcg 5 1107 400 56 59 2 gre_1107.mtx >> BICGintoX56to59rangeNis400/56to59result_gre_1107.txt
+mkdir BICGintoX56to59rangeNis600
+./mainbcg 5 1107 600 56 59 2 gre_1107.mtx >> BICGintoX56to59rangeNis600/56to59result_gre_1107.txt
+mkdir BICGintoX56to59rangeNis800
+./mainbcg 5 1107 800 56 59 2 gre_1107.mtx >> BICGintoX56to59rangeNis800/56to59result_gre_1107.txt
+mkdir BICGintoR52to55rangeNis200
+./mainbcg 5 1107 200 52 55 0 gre_1107.mtx >> BICGintoR52to55rangeNis200/52to55result_gre_1107.txt
+mkdir BICGintoR52to55rangeNis400
+./mainbcg 5 1107 400 52 55 0 gre_1107.mtx >> BICGintoR52to55rangeNis400/52to55result_gre_1107.txt
+mkdir BICGintoR52to55rangeNis600
+./mainbcg 5 1107 600 52 55 0 gre_1107.mtx >> BICGintoR52to55rangeNis600/52to55result_gre_1107.txt
+mkdir BICGintoR52to55rangeNis800
+./mainbcg 5 1107 800 52 55 0 gre_1107.mtx >> BICGintoR52to55rangeNis800/52to55result_gre_1107.txt
+mkdir BICGintoP52to55rangeNis200
+./mainbcg 5 1107 200 52 55 1 gre_1107.mtx >> BICGintoP52to55rangeNis200/52to55result_gre_1107.txt
+mkdir BICGintoP52to55rangeNis400
+./mainbcg 5 1107 400 52 55 1 gre_1107.mtx >> BICGintoP52to55rangeNis400/52to55result_gre_1107.txt
+mkdir BICGintoP52to55rangeNis600
+./mainbcg 5 1107 600 52 55 1 gre_1107.mtx >> BICGintoP52to55rangeNis600/52to55result_gre_1107.txt
+mkdir BICGintoP52to55rangeNis800
+./mainbcg 5 1107 800 52 55 1 gre_1107.mtx >> BICGintoP52to55rangeNis800/52to55result_gre_1107.txt
+mkdir BICGintoX60to63rangeNis200
+./mainbcg 5 1107 200 52 55 2 gre_1107.mtx >> BICGintoX52to55rangeNis200/52to55result_gre_1107.txt
+mkdir BICGintoX60to63rangeNis400
+./mainbcg 5 1107 400 52 55 2 gre_1107.mtx >> BICGintoX52to55rangeNis400/52to55result_gre_1107.txt
+mkdir BICGintoX60to63rangeNis600
+./mainbcg 5 1107 600 52 55 2 gre_1107.mtx >> BICGintoX52to55rangeNis600/52to55result_gre_1107.txt
+mkdir BICGintoX60to63rangeNis800
+./mainbcg 5 1107 800 52 55 2 gre_1107.mtx >> BICGintoX52to55rangeNis800/52to55result_gre_1107.txt
