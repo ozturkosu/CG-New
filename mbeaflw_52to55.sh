28c90d0d5e503216168529ccd9491dd35fe8fcd6
ozturkosu <ozturk.osu@gmail.com>
2019-04-30 06:08:37 -0400

    a

---
+++ mbeaflw_52to55.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir BICGintoR52to55rangeNis200
+./mainbcg 5 512 100 52 55 0 mbeaflw.mtx >> BICGintoR52to55rangeNis200/52to55result_mbeaflw.txt
+mkdir BICGintoR52to55rangeNis400
+./mainbcg 5 512 200 52 55 0 mbeaflw.mtx >> BICGintoR52to55rangeNis400/52to55result_mbeaflw.txt
+mkdir BICGintoR52to55rangeNis600
+./mainbcg 5 512 300 52 55 0 mbeaflw.mtx >> BICGintoR52to55rangeNis600/52to55result_mbeaflw.txt
+mkdir BICGintoR52to55rangeNis800
+./mainbcg 5 512 400 52 55 0 mbeaflw.mtx >> BICGintoR52to55rangeNis800/52to55result_mbeaflw.txt
+mkdir BICGintoP52to55rangeNis200
+./mainbcg 5 512 100 52 55 1 mbeaflw.mtx >> BICGintoP52to55rangeNis200/52to55result_mbeaflw.txt
+mkdir BICGintoP52to55rangeNis400
+./mainbcg 5 512 200 52 55 1 mbeaflw.mtx >> BICGintoP52to55rangeNis400/52to55result_mbeaflw.txt
+mkdir BICGintoP52to55rangeNis600
+./mainbcg 5 512 300 52 55 1 mbeaflw.mtx >> BICGintoP52to55rangeNis600/52to55result_mbeaflw.txt
+mkdir BICGintoP52to55rangeNis800
+./mainbcg 5 512 400 52 55 1 mbeaflw.mtx >> BICGintoP52to55rangeNis800/52to55result_mbeaflw.txt
+mkdir BICGintoX52to55rangeNis200
+./mainbcg 5 512 100 52 55 2 mbeaflw.mtx >> BICGintoX52to55rangeNis200/52to55result_mbeaflw.txt
+mkdir BICGintoX52to55rangeNis400
+./mainbcg 5 512 200 52 55 2 mbeaflw.mtx >> BICGintoX52to55rangeNis400/52to55result_mbeaflw.txt
+mkdir BICGintoX52to55rangeNis600
+./mainbcg 5 512 300 52 55 2 mbeaflw.mtx >> BICGintoX52to55rangeNis600/52to55result_mbeaflw.txt
+mkdir BICGintoX52to55rangeNis800
+./mainbcg 5 512 400 52 55 2 mbeaflw.mtx >> BICGintoX52to55rangeNis800/52to55result_mbeaflw.txt
