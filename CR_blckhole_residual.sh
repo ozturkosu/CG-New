bab9fb9d6d4b128c9d20bff381ea891da15c2cff
ozturkosu <ozturk.osu@gmail.com>
2019-06-19 01:46:21 -0400

    sh

---
+++ CR_blckhole_residual.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir CRintoR60to63rangeNis200
+./maincr1 5 2132 400 60 63 0 blckhole.mtx >> CRintoR60to63rangeNis200/residual60to63result_blckhole.txt
+mkdir CRintoR60to63rangeNis400
+./maincr1 5 2132 800 60 63 0 blckhole.mtx >> CRintoR60to63rangeNis400/residual60to63result_blckhole.txt
+mkdir CRintoR60to63rangeNis600
+./maincr1 5 2132 1200 60 63 0 blckhole.mtx >> CRintoR60to63rangeNis600/residual60to63result_blckhole.txt
+mkdir CRintoR60to63rangeNis800
+./maincr1 5 2132 1600 60 63 0 blckhole.mtx >> CRintoR60to63rangeNis800/residual60to63result_blckhole.txt
+mkdir CRintoP60to63rangeNis200
+./maincr1 5 2132 400 60 63 1 blckhole.mtx >> CRintoP60to63rangeNis200/residual60to63result_blckhole.txt
+mkdir CRintoP60to63rangeNis400
+./maincr1 5 2132 800 60 63 1 blckhole.mtx >> CRintoP60to63rangeNis400/residual60to63result_blckhole.txt
+mkdir CRintoP60to63rangeNis600
+./maincr1 5 2132 1200 60 63 1 blckhole.mtx >> CRintoP60to63rangeNis600/residual60to63result_blckhole.txt
+mkdir CRintoP60to63rangeNis800
+./maincr1 5 2132 1600 60 63 1 blckhole.mtx >> CRintoP60to63rangeNis800/residual60to63result_blckhole.txt
+mkdir CRintoX60to63rangeNis200
+./maincr1 5 2132 400 60 63 2 blckhole.mtx >> CRintoX60to63rangeNis200/residual60to63result_blckhole.txt
+mkdir CRintoX60to63rangeNis400
+./maincr1 5 2132 800 60 63 2 blckhole.mtx >> CRintoX60to63rangeNis400/residual60to63result_blckhole.txt
+mkdir CRintoX60to63rangeNis600
+./maincr1 5 2132 1200 60 63 2 blckhole.mtx >> CRintoX60to63rangeNis600/residual60to63result_blckhole.txt
+mkdir CRintoX60to63rangeNis800
+./maincr1 5 2132 1600 60 63 2 blckhole.mtx >> CRintoX60to63rangeNis800/residual60to63result_blckhole.txt
+mkdir CRintoR56to59rangeNis200
+./maincr1 5 2132 400 56 59 0 blckhole.mtx >> CRintoR56to59rangeNis200/residual56to63result_blckhole.txt
+mkdir CRintoR56to59rangeNis400
+./maincr1 5 2132 800 56 59 0 blckhole.mtx >> CRintoR56to59rangeNis400/residual56to63result_blckhole.txt
+mkdir CRintoR56to59rangeNis600
+./maincr1 5 2132 1200 56 59 0 blckhole.mtx >> CRintoR56to59rangeNis600/residual56to63result_blckhole.txt
+mkdir CRintoR56to59rangeNis800
+./maincr1 5 2132 1600 56 59 0 blckhole.mtx >> CRintoR56to59rangeNis800/residual56to63result_blckhole.txt
+mkdir CRintoP56to59rangeNis200
+./maincr1 5 2132 400 56 59 1 blckhole.mtx >> CRintoP56to59rangeNis200/residual56to63result_blckhole.txt
+mkdir CRintoP56to59rangeNis400
+./maincr1 5 2132 800 56 59 1 blckhole.mtx >> CRintoP56to59rangeNis400/residual56to63result_blckhole.txt
+mkdir CRintoP56to59rangeNis600
+./maincr1 5 2132 1200 56 59 1 blckhole.mtx >> CRintoP56to59rangeNis600/residual56to63result_blckhole.txt
+mkdir CRintoP56to59rangeNis800
+./maincr1 5 2132 1600 56 59 1 blckhole.mtx >> CRintoP56to59rangeNis800/residual56to63result_blckhole.txt
+mkdir CRintoX56to59rangeNis200
+./maincr1 5 2132 400 56 59 2 blckhole.mtx >> CRintoX56to59rangeNis200/residual56to63result_blckhole.txt
+mkdir CRintoX56to59rangeNis400
+./maincr1 5 2132 800 56 59 2 blckhole.mtx >> CRintoX56to59rangeNis400/residual56to63result_blckhole.txt
+mkdir CRintoX56to59rangeNis600
+./maincr1 5 2132 1200 56 59 2 blckhole.mtx >> CRintoX56to59rangeNis600/residual56to63result_blckhole.txt
+mkdir CRintoX56to59rangeNis800
+./maincr1 5 2132 1600 56 59 2 blckhole.mtx >> CRintoX56to59rangeNis800/residual56to63result_blckhole.txt
+mkdir CRintoR52to55rangeNis200
+./maincr1 5 2132 400 52 55 0 blckhole.mtx >> CRintoR52to55rangeNis200/residual52to55result_blckhole.txt
+mkdir CRintoR52to55rangeNis400
+./maincr1 5 2132 800 52 55 0 blckhole.mtx >> CRintoR52to55rangeNis400/residual52to55result_blckhole.txt
+mkdir CRintoR52to55rangeNis600
+./maincr1 5 2132 1200 52 55 0 blckhole.mtx >> CRintoR52to55rangeNis600/residual52to55result_blckhole.txt
+mkdir CRintoR52to55rangeNis800
+./maincr1 5 2132 1600 52 55 0 blckhole.mtx >> CRintoR52to55rangeNis800/residual52to55result_blckhole.txt
+mkdir CRintoP52to55rangeNis200
+./maincr1 5 2132 400 52 55 1 blckhole.mtx >> CRintoP52to55rangeNis200/residual52to55result_blckhole.txt
+mkdir CRintoP52to55rangeNis400
+./maincr1 5 2132 800 52 55 1 blckhole.mtx >> CRintoP52to55rangeNis400/residual52to55result_blckhole.txt
+mkdir CRintoP52to55rangeNis600
+./maincr1 5 2132 1200 52 55 1 blckhole.mtx >> CRintoP52to55rangeNis600/residual52to55result_blckhole.txt
+mkdir CRintoP52to55rangeNis800
+./maincr1 5 2132 1600 52 55 1 blckhole.mtx >> CRintoP52to55rangeNis800/residual52to55result_blckhole.txt
+mkdir CRintoX52to55rangeNis200
+./maincr1 5 2132 400 52 55 2 blckhole.mtx >> CRintoX52to55rangeNis200/residual52to55result_blckhole.txt
+mkdir CRintoX52to55rangeNis400
+./maincr1 5 2132 800 52 55 2 blckhole.mtx >> CRintoX52to55rangeNis400/residual52to55result_blckhole.txt
+mkdir CRintoX52to55rangeNis600
+./maincr1 5 2132 1200 52 55 2 blckhole.mtx >> CRintoX52to55rangeNis600/residual52to55result_blckhole.txt
+mkdir CRintoX52to55rangeNis800
+./maincr1 5 2132 1600 52 55 2 blckhole.mtx >> CRintoX52to55rangeNis800/residual52to55result_blckhole.txt
+mkdir CRintoR48to51rangeNis200
+./maincr1 5 2132 400 48 51 0 blckhole.mtx >> CRintoR48to51rangeNis200/residual48to51result_blckhole.txt
+mkdir CRintoR48to51rangeNis400
+./maincr1 5 2132 800 48 51 0 blckhole.mtx >> CRintoR48to51rangeNis400/residual48to51result_blckhole.txt
+mkdir CRintoR48to51rangeNis600
+./maincr1 5 2132 1200 48 51 0 blckhole.mtx >> CRintoR48to51rangeNis600/residual48to51result_blckhole.txt
+mkdir CRintoR48to51rangeNis800
+./maincr1 5 2132 1600 48 51 0 blckhole.mtx >> CRintoR48to51rangeNis800/residual48to51result_blckhole.txt
+mkdir CRintoP48to51rangeNis200
+./maincr1 5 2132 400 48 51 1 blckhole.mtx >> CRintoP48to51rangeNis200/residual48to51result_blckhole.txt
+mkdir CRintoP48to51rangeNis400
+./maincr1 5 2132 800 48 51 1 blckhole.mtx >> CRintoP48to51rangeNis400/residual48to51result_blckhole.txt
+mkdir CRintoP48to51rangeNis600
+./maincr1 5 2132 1200 48 51 1 blckhole.mtx >> CRintoP48to51rangeNis600/residual48to51result_blckhole.txt
+mkdir CRintoP48to51rangeNis800
+./maincr1 5 2132 1600 48 51 1 blckhole.mtx >> CRintoP48to51rangeNis800/residual48to51result_blckhole.txt
+mkdir CRintoX48to51rangeNis200
+./maincr1 5 2132 400 48 51 2 blckhole.mtx >> CRintoX48to51rangeNis200/residual48to51result_blckhole.txt
+mkdir CRintoX48to51rangeNis400
+./maincr1 5 2132 800 48 51 2 blckhole.mtx >> CRintoX48to51rangeNis400/residual48to51result_blckhole.txt
+mkdir CRintoX48to51rangeNis600
+./maincr1 5 2132 1200 48 51 2 blckhole.mtx >> CRintoX48to51rangeNis600/residual48to51result_blckhole.txt
+mkdir CRintoX48to51rangeNis800
+./maincr1 5 2132 1600 48 51 2 blckhole.mtx >> CRintoX48to51rangeNis800/residual48to51result_blckhole.txt
+ 
