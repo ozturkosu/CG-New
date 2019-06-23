613ad59fc3f58eb4c55d295228c06e8e7e59f2d3
ozturkosu <ozturk.osu@gmail.com>
2019-06-22 22:23:47 -0400

    new sh

---
+++ CR_bcsstk06.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir CRintoR60to63rangeNis200
+./maincr1 5 420 84 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis200/residual60to63result_bcsstk06.txt
+mkdir CRintoR60to63rangeNis400
+./maincr1 5 420 168 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis400/residual60to63result_bcsstk06.txt
+mkdir CRintoR60to63rangeNis600
+./maincr1 5 420 252 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis600/residual60to63result_bcsstk06.txt
+mkdir CRintoR60to63rangeNis800
+./maincr1 5 420 336 60 63 0 bcsstk06.mtx >> CRintoR60to63rangeNis800/residual60to63result_bcsstk06.txt
+mkdir CRintoP60to63rangeNis200
+./maincr1 5 420 84 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis200/residual60to63result_bcsstk06.txt
+mkdir CRintoP60to63rangeNis400
+./maincr1 5 420 168 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis400/residual60to63result_bcsstk06.txt
+mkdir CRintoP60to63rangeNis600
+./maincr1 5 420 252 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis600/residual60to63result_bcsstk06.txt
+mkdir CRintoP60to63rangeNis800
+./maincr1 5 420 336 60 63 1 bcsstk06.mtx >> CRintoP60to63rangeNis800/residual60to63result_bcsstk06.txt
+mkdir CRintoX60to63rangeNis200
+./maincr1 5 420 84 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis200/residual60to63result_bcsstk06.txt
+mkdir CRintoX60to63rangeNis400
+./maincr1 5 420 168 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis400/residual60to63result_bcsstk06.txt
+mkdir CRintoX60to63rangeNis600
+./maincr1 5 420 252 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis600/residual60to63result_bcsstk06.txt
+mkdir CRintoX60to63rangeNis800
+./maincr1 5 420 336 60 63 2 bcsstk06.mtx >> CRintoX60to63rangeNis800/residual60to63result_bcsstk06.txt
+mkdir CRintoR56to59rangeNis200
+./maincr1 5 420 84 56 59 0 bcsstk06.mtx >> CRintoR56to59rangeNis200/residual56to63result_bcsstk06.txt
+mkdir CRintoR56to59rangeNis400
+./maincr1 5 420 168 56 59 0 bcsstk06.mtx >> CRintoR56to59rangeNis400/residual56to63result_bcsstk06.txt
+mkdir CRintoR56to59rangeNis600
+./maincr1 5 420 252 56 59 0 bcsstk06.mtx >> CRintoR56to59rangeNis600/residual56to63result_bcsstk06.txt
+mkdir CRintoR56to59rangeNis800
+./maincr1 5 420 336 56 59 0 bcsstk06.mtx >> CRintoR56to59rangeNis800/residual56to63result_bcsstk06.txt
+mkdir CRintoP56to59rangeNis200
+./maincr1 5 420 84 56 59 1 bcsstk06.mtx >> CRintoP56to59rangeNis200/residual56to63result_bcsstk06.txt
+mkdir CRintoP56to59rangeNis400
+./maincr1 5 420 168 56 59 1 bcsstk06.mtx >> CRintoP56to59rangeNis400/residual56to63result_bcsstk06.txt
+mkdir CRintoP56to59rangeNis600
+./maincr1 5 420 252 56 59 1 bcsstk06.mtx >> CRintoP56to59rangeNis600/residual56to63result_bcsstk06.txt
+mkdir CRintoP56to59rangeNis800
+./maincr1 5 420 336 56 59 1 bcsstk06.mtx >> CRintoP56to59rangeNis800/residual56to63result_bcsstk06.txt
+mkdir CRintoX56to59rangeNis200
+./maincr1 5 420 84 56 59 2 bcsstk06.mtx >> CRintoX56to59rangeNis200/residual56to63result_bcsstk06.txt
+mkdir CRintoX56to59rangeNis400
+./maincr1 5 420 168 56 59 2 bcsstk06.mtx >> CRintoX56to59rangeNis400/residual56to63result_bcsstk06.txt
+mkdir CRintoX56to59rangeNis600
+./maincr1 5 420 252 56 59 2 bcsstk06.mtx >> CRintoX56to59rangeNis600/residual56to63result_bcsstk06.txt
+mkdir CRintoX56to59rangeNis800
+./maincr1 5 420 336 56 59 2 bcsstk06.mtx >> CRintoX56to59rangeNis800/residual56to63result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis200
+./maincr1 5 420 84 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis200/residual52to55result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis400
+./maincr1 5 420 168 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis400/residual52to55result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis600
+./maincr1 5 420 252 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis600/residual52to55result_bcsstk06.txt
+mkdir CRintoR52to55rangeNis800
+./maincr1 5 420 336 52 55 0 bcsstk06.mtx >> CRintoR52to55rangeNis800/residual52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis200
+./maincr1 5 420 84 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis200/residual52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis400
+./maincr1 5 420 168 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis400/residual52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis600
+./maincr1 5 420 252 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis600/residual52to55result_bcsstk06.txt
+mkdir CRintoP52to55rangeNis800
+./maincr1 5 420 336 52 55 1 bcsstk06.mtx >> CRintoP52to55rangeNis800/residual52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis200
+./maincr1 5 420 84 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis200/residual52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis400
+./maincr1 5 420 168 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis400/residual52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis600
+./maincr1 5 420 252 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis600/residual52to55result_bcsstk06.txt
+mkdir CRintoX52to55rangeNis800
+./maincr1 5 420 336 52 55 2 bcsstk06.mtx >> CRintoX52to55rangeNis800/residual52to55result_bcsstk06.txt
+mkdir CRintoR48to51rangeNis200
+./maincr1 5 420 84 48 51 0 bcsstk06.mtx >> CRintoR48to51rangeNis200/residual48to51result_bcsstk06.txt
+mkdir CRintoR48to51rangeNis400
+./maincr1 5 420 168 48 51 0 bcsstk06.mtx >> CRintoR48to51rangeNis400/residual48to51result_bcsstk06.txt
+mkdir CRintoR48to51rangeNis600
+./maincr1 5 420 252 48 51 0 bcsstk06.mtx >> CRintoR48to51rangeNis600/residual48to51result_bcsstk06.txt
+mkdir CRintoR48to51rangeNis800
+./maincr1 5 420 336 48 51 0 bcsstk06.mtx >> CRintoR48to51rangeNis800/residual48to51result_bcsstk06.txt
+mkdir CRintoP48to51rangeNis200
+./maincr1 5 420 84 48 51 1 bcsstk06.mtx >> CRintoP48to51rangeNis200/residual48to51result_bcsstk06.txt
+mkdir CRintoP48to51rangeNis400
+./maincr1 5 420 168 48 51 1 bcsstk06.mtx >> CRintoP48to51rangeNis400/residual48to51result_bcsstk06.txt
+mkdir CRintoP48to51rangeNis600
+./maincr1 5 420 252 48 51 1 bcsstk06.mtx >> CRintoP48to51rangeNis600/residual48to51result_bcsstk06.txt
+mkdir CRintoP48to51rangeNis800
+./maincr1 5 420 336 48 51 1 bcsstk06.mtx >> CRintoP48to51rangeNis800/residual48to51result_bcsstk06.txt
+mkdir CRintoX48to51rangeNis200
+./maincr1 5 420 84 48 51 2 bcsstk06.mtx >> CRintoX48to51rangeNis200/residual48to51result_bcsstk06.txt
+mkdir CRintoX48to51rangeNis400
+./maincr1 5 420 168 48 51 2 bcsstk06.mtx >> CRintoX48to51rangeNis400/residual48to51result_bcsstk06.txt
+mkdir CRintoX48to51rangeNis600
+./maincr1 5 420 252 48 51 2 bcsstk06.mtx >> CRintoX48to51rangeNis600/residual48to51result_bcsstk06.txt
+mkdir CRintoX48to51rangeNis800
+./maincr1 5 420 336 48 51 2 bcsstk06.mtx >> CRintoX48to51rangeNis800/residual48to51result_bcsstk06.txt
+ 
