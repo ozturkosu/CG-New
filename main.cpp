00ed47e53d2c18e45becc47ef657b1e8568cf32c
ozturkosu <ozturk.osu@gmail.com>
2019-06-25 00:21:19 -0400

    A

--- main.cpp
+++ main.cpp
@@ -70,8 +70,8 @@ int main (int argc, char** argv)
     cout << "SuccessfulRate = " << Global::successfulRate << " % " <<endl ;
    
 
-    //r8ge_cg_start_withNewIndicator(windowSize, psize, threshold, fPos , range1 , range2 , k ) ;
-    r8ge_gcr_start_withResidual ( windowSize, psize, threshold, fPos , range1 , range2 , k , matrixname);
+    r8ge_cg_start_withNewIndicator(windowSize, psize, threshold, fPos , range1 , range2 , k ) ;
+    //r8ge_gcr_start_withResidual ( windowSize, psize, threshold, fPos , range1 , range2 , k , matrixname);
     //r8ge_bcg_start_withIndicator( windowSize, psize, threshold, fPos , range1 , range2 , k , matrixname);
     //r8ge_bcr_start_withIndicator( windowSize, psize, threshold, fPos , range1 , range2 , k , matrixname);
   }
