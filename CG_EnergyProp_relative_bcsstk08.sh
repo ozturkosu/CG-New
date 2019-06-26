dcbaaefa0675c5b05ba97605f1d5c89c2df81f6c
ozturkosu <ozturk.osu@gmail.com>
2019-06-26 16:54:08 -0400

    a

---
+++ CG_EnergyProp_relative_bcsstk08.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+mkdir IntoR60to63rangeNis200
+./mainCGEnergyRelative 5 1074 214 60 63 0 bcsstk08.mtx >> IntoR60to63rangeNis200/relative_bcsstk08.txt
+mkdir IntoR60to63rangeNis400
+./mainCGEnergyRelative 5 1074 428 60 63 0 bcsstk08.mtx >> IntoR60to63rangeNis400/relative_bcsstk08.txt
+mkdir IntoR60to63rangeNis600
+./mainCGEnergyRelative 5 1074 645 60 63 0 bcsstk08.mtx >> IntoR60to63rangeNis600/relative_bcsstk08.txt
+mkdir IntoR60to63rangeNis800
+./mainCGEnergyRelative 5 1074 860 60 63 0 bcsstk08.mtx >> IntoR60to63rangeNis800/relative_bcsstk08.txt
+mkdir IntoP60to63rangeNis200
+./mainCGEnergyRelative 5 1074 214 60 63 1 bcsstk08.mtx >> IntoP60to63rangeNis200/relative_bcsstk08.txt
+mkdir IntoP60to63rangeNis400
+./mainCGEnergyRelative 5 1074 428 60 63 1 bcsstk08.mtx >> IntoP60to63rangeNis400/relative_bcsstk08.txt
+mkdir IntoP60to63rangeNis600
+./mainCGEnergyRelative 5 1074 645 60 63 1 bcsstk08.mtx >> IntoP60to63rangeNis600/relative_bcsstk08.txt
+mkdir IntoP60to63rangeNis800
+./mainCGEnergyRelative 5 1074 860 60 63 1 bcsstk08.mtx >> IntoP60to63rangeNis800/relative_bcsstk08.txt
+mkdir IntoX60to63rangeNis200
+./mainCGEnergyRelative 5 1074 214 60 63 2 bcsstk08.mtx >> IntoX60to63rangeNis200/relative_bcsstk08.txt
+mkdir IntoX60to63rangeNis400
+./mainCGEnergyRelative 5 1074 428 60 63 2 bcsstk08.mtx >> IntoX60to63rangeNis400/relative_bcsstk08.txt
+mkdir IntoX60to63rangeNis600
+./mainCGEnergyRelative 5 1074 645 60 63 2 bcsstk08.mtx >> IntoX60to63rangeNis600/relative_bcsstk08.txt
+mkdir IntoX60to63rangeNis800
+./mainCGEnergyRelative 5 1074 860 60 63 2 bcsstk08.mtx >> IntoX60to63rangeNis800/relative_bcsstk08.txt
+mkdir IntoR56to59rangeNis200
+./mainCGEnergyRelative 5 1074 214 56 59 0 bcsstk08.mtx >> IntoR56to59rangeNis200/relative_bcsstk08.txt
+mkdir IntoR56to59rangeNis400
+./mainCGEnergyRelative 5 1074 428 56 59 0 bcsstk08.mtx >> IntoR56to59rangeNis400/relative_bcsstk08.txt
+mkdir IntoR56to59rangeNis600
+./mainCGEnergyRelative 5 1074 645 56 59 0 bcsstk08.mtx >> IntoR56to59rangeNis600/relative_bcsstk08.txt
+mkdir IntoR56to59rangeNis800
+./mainCGEnergyRelative 5 1074 860 56 59 0 bcsstk08.mtx >> IntoR56to59rangeNis800/relative_bcsstk08.txt
+mkdir IntoP56to59rangeNis200
+./mainCGEnergyRelative 5 1074 214 56 59 1 bcsstk08.mtx >> IntoP56to59rangeNis200/relative_bcsstk08.txt
+mkdir IntoP56to59rangeNis400
+./mainCGEnergyRelative 5 1074 428 56 59 1 bcsstk08.mtx >> IntoP56to59rangeNis400/relative_bcsstk08.txt
+mkdir IntoP56to59rangeNis600
+./mainCGEnergyRelative 5 1074 645 56 59 1 bcsstk08.mtx >> IntoP56to59rangeNis600/relative_bcsstk08.txt
+mkdir IntoP56to59rangeNis800
+./mainCGEnergyRelative 5 1074 860 56 59 1 bcsstk08.mtx >> IntoP56to59rangeNis800/relative_bcsstk08.txt
+mkdir IntoX56to59rangeNis200
+./mainCGEnergyRelative 5 1074 214 56 59 2 bcsstk08.mtx >> IntoX56to59rangeNis200/relative_bcsstk08.txt
+mkdir IntoX56to59rangeNis400
+./mainCGEnergyRelative 5 1074 428 56 59 2 bcsstk08.mtx >> IntoX56to59rangeNis400/relative_bcsstk08.txt
+mkdir IntoX56to59rangeNis600
+./mainCGEnergyRelative 5 1074 645 56 59 2 bcsstk08.mtx >> IntoX56to59rangeNis600/relative_bcsstk08.txt
+mkdir IntoX56to59rangeNis800
+./mainCGEnergyRelative 5 1074 860 56 59 2 bcsstk08.mtx >> IntoX56to59rangeNis800/relative_bcsstk08.txt
+mkdir IntoR52to55rangeNis200
+./mainCGEnergyRelative 5 1074 214 52 55 0 bcsstk08.mtx >> IntoR52to55rangeNis200/relative_bcsstk08.txt
+mkdir IntoR52to55rangeNis400
+./mainCGEnergyRelative 5 1074 428 52 55 0 bcsstk08.mtx >> IntoR52to55rangeNis400/relative_bcsstk08.txt
+mkdir IntoR52to55rangeNis600
+./mainCGEnergyRelative 5 1074 645 52 55 0 bcsstk08.mtx >> IntoR52to55rangeNis600/relative_bcsstk08.txt
+mkdir IntoR52to55rangeNis800
+./mainCGEnergyRelative 5 1074 860 52 55 0 bcsstk08.mtx >> IntoR52to55rangeNis800/relative_bcsstk08.txt
+mkdir IntoP52to55rangeNis200
+./mainCGEnergyRelative 5 1074 214 52 55 1 bcsstk08.mtx >> IntoP52to55rangeNis200/relative_bcsstk08.txt
+mkdir IntoP52to55rangeNis400
+./mainCGEnergyRelative 5 1074 428 52 55 1 bcsstk08.mtx >> IntoP52to55rangeNis400/relative_bcsstk08.txt
+mkdir IntoP52to55rangeNis600
+./mainCGEnergyRelative 5 1074 645 52 55 1 bcsstk08.mtx >> IntoP52to55rangeNis600/relative_bcsstk08.txt
+mkdir IntoP52to55rangeNis800
+./mainCGEnergyRelative 5 1074 860 52 55 1 bcsstk08.mtx >> IntoP52to55rangeNis800/relative_bcsstk08.txt
+mkdir IntoX52to55rangeNis200
+./mainCGEnergyRelative 5 1074 214 52 55 2 bcsstk08.mtx >> IntoX52to55rangeNis200/relative_bcsstk08.txt
+mkdir IntoX52to55rangeNis400
+./mainCGEnergyRelative 5 1074 428 52 55 2 bcsstk08.mtx >> IntoX52to55rangeNis400/relative_bcsstk08.txt
+mkdir IntoX52to55rangeNis600
+./mainCGEnergyRelative 5 1074 645 52 55 2 bcsstk08.mtx >> IntoX52to55rangeNis600/relative_bcsstk08.txt
+mkdir IntoX52to55rangeNis800
+./mainCGEnergyRelative 5 1074 860 52 55 2 bcsstk08.mtx >> IntoX52to55rangeNis800/relative_bcsstk08.txt
+mkdir IntoR48to51rangeNis200
+./mainCGEnergyRelative 5 1074 214 48 51 0 bcsstk08.mtx >> IntoR48to51rangeNis200/relative_bcsstk08.txt
+mkdir IntoR48to51rangeNis400
+./mainCGEnergyRelative 5 1074 428 48 51 0 bcsstk08.mtx >> IntoR48to51rangeNis400/relative_bcsstk08.txt
+mkdir IntoR48to51rangeNis600
+./mainCGEnergyRelative 5 1074 645 48 51 0 bcsstk08.mtx >> IntoR48to51rangeNis600/relative_bcsstk08.txt
+mkdir IntoR48to51rangeNis800
+./mainCGEnergyRelative 5 1074 860 48 51 0 bcsstk08.mtx >> IntoR48to51rangeNis800/relative_bcsstk08.txt
+mkdir IntoP48to51rangeNis200
+./mainCGEnergyRelative 5 1074 214 48 51 1 bcsstk08.mtx >> IntoP48to51rangeNis200/relative_bcsstk08.txt
+mkdir IntoP48to51rangeNis400
+./mainCGEnergyRelative 5 1074 428 48 51 1 bcsstk08.mtx >> IntoP48to51rangeNis400/relative_bcsstk08.txt
+mkdir IntoP48to51rangeNis600
+./mainCGEnergyRelative 5 1074 645 48 51 1 bcsstk08.mtx >> IntoP48to51rangeNis600/relative_bcsstk08.txt
+mkdir IntoP48to51rangeNis800
+./mainCGEnergyRelative 5 1074 860 48 51 1 bcsstk08.mtx >> IntoP48to51rangeNis800/relative_bcsstk08.txt
+mkdir IntoX48to51rangeNis200
+./mainCGEnergyRelative 5 1074 214 48 51 2 bcsstk08.mtx >> IntoX48to51rangeNis200/relative_bcsstk08.txt
+mkdir IntoX48to51rangeNis400
+./mainCGEnergyRelative 5 1074 428 48 51 2 bcsstk08.mtx >> IntoX48to51rangeNis400/relative_bcsstk08.txt
+mkdir IntoX48to51rangeNis600
+./mainCGEnergyRelative 5 1074 645 48 51 2 bcsstk08.mtx >> IntoX48to51rangeNis600/relative_bcsstk08.txt
+mkdir IntoX48to51rangeNis800
+./mainCGEnergyRelative 5 1074 860 48 51 2 bcsstk08.mtx >> IntoX48to51rangeNis800/relative_bcsstk08.txt
+ 
