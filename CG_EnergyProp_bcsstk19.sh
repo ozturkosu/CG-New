eb41d875aeae32d965837728b03fc2a92d8510f0
ozturkosu <ozturk.osu@gmail.com>
2019-06-25 23:17:39 -0400

    a

--- CG_EnergyProp_bcsstk19.sh
+++ CG_EnergyProp_bcsstk19.sh
@@ -1,99 +1,99 @@
 #!/bin/bash
 #SBATCH --time=23:59:00
-mkdir CG_EnergyIntoR60to63rangeNis200
-./mainCGEnergy2 5 729 144 60 63 0 bcsstk19.mtx >> CG_EnergyIntoR60to63rangeNis200/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoR60to63rangeNis400
-./mainCGEnergy2 5 729 288 60 63 0 bcsstk19.mtx >> CG_EnergyIntoR60to63rangeNis400/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoR60to63rangeNis600
-./mainCGEnergy2 5 729 450 60 63 0 bcsstk19.mtx >> CG_EnergyIntoR60to63rangeNis600/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoR60to63rangeNis800
-./mainCGEnergy2 5 729 572 60 63 0 bcsstk19.mtx >> CG_EnergyIntoR60to63rangeNis800/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoP60to63rangeNis200
-./mainCGEnergy2 5 729 144 60 63 1 bcsstk19.mtx >> CG_EnergyIntoP60to63rangeNis200/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoP60to63rangeNis400
-./mainCGEnergy2 5 729 288 60 63 1 bcsstk19.mtx >> CG_EnergyIntoP60to63rangeNis400/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoP60to63rangeNis600
-./mainCGEnergy2 5 729 450 60 63 1 bcsstk19.mtx >> CG_EnergyIntoP60to63rangeNis600/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoP60to63rangeNis800
-./mainCGEnergy2 5 729 572 60 63 1 bcsstk19.mtx >> CG_EnergyIntoP60to63rangeNis800/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoX60to63rangeNis200
-./mainCGEnergy2 5 729 144 60 63 2 bcsstk19.mtx >> CG_EnergyIntoX60to63rangeNis200/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoX60to63rangeNis400
-./mainCGEnergy2 5 729 288 60 63 2 bcsstk19.mtx >> CG_EnergyIntoX60to63rangeNis400/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoX60to63rangeNis600
-./mainCGEnergy2 5 729 450 60 63 2 bcsstk19.mtx >> CG_EnergyIntoX60to63rangeNis600/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoX60to63rangeNis800
-./mainCGEnergy2 5 729 572 60 63 2 bcsstk19.mtx >> CG_EnergyIntoX60to63rangeNis800/EnergyProp60to63result_bcsstk19.txt
-mkdir CG_EnergyIntoR56to59rangeNis200
-./mainCGEnergy2 5 729 144 56 59 0 bcsstk19.mtx >> CG_EnergyIntoR56to59rangeNis200/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoR56to59rangeNis400
-./mainCGEnergy2 5 729 288 56 59 0 bcsstk19.mtx >> CG_EnergyIntoR56to59rangeNis400/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoR56to59rangeNis600
-./mainCGEnergy2 5 729 450 56 59 0 bcsstk19.mtx >> CG_EnergyIntoR56to59rangeNis600/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoR56to59rangeNis800
-./mainCGEnergy2 5 729 572 56 59 0 bcsstk19.mtx >> CG_EnergyIntoR56to59rangeNis800/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoP56to59rangeNis200
-./mainCGEnergy2 5 729 144 56 59 1 bcsstk19.mtx >> CG_EnergyIntoP56to59rangeNis200/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoP56to59rangeNis400
-./mainCGEnergy2 5 729 288 56 59 1 bcsstk19.mtx >> CG_EnergyIntoP56to59rangeNis400/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoP56to59rangeNis600
-./mainCGEnergy2 5 729 450 56 59 1 bcsstk19.mtx >> CG_EnergyIntoP56to59rangeNis600/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoP56to59rangeNis800
-./mainCGEnergy2 5 729 572 56 59 1 bcsstk19.mtx >> CG_EnergyIntoP56to59rangeNis800/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoX56to59rangeNis200
-./mainCGEnergy2 5 729 144 56 59 2 bcsstk19.mtx >> CG_EnergyIntoX56to59rangeNis200/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoX56to59rangeNis400
-./mainCGEnergy2 5 729 288 56 59 2 bcsstk19.mtx >> CG_EnergyIntoX56to59rangeNis400/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoX56to59rangeNis600
-./mainCGEnergy2 5 729 450 56 59 2 bcsstk19.mtx >> CG_EnergyIntoX56to59rangeNis600/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoX56to59rangeNis800
-./mainCGEnergy2 5 729 572 56 59 2 bcsstk19.mtx >> CG_EnergyIntoX56to59rangeNis800/EnergyProp56to63result_bcsstk19.txt
-mkdir CG_EnergyIntoR52to55rangeNis200
-./mainCGEnergy2 5 729 144 52 55 0 bcsstk19.mtx >> CG_EnergyIntoR52to55rangeNis200/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoR52to55rangeNis400
-./mainCGEnergy2 5 729 288 52 55 0 bcsstk19.mtx >> CG_EnergyIntoR52to55rangeNis400/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoR52to55rangeNis600
-./mainCGEnergy2 5 729 450 52 55 0 bcsstk19.mtx >> CG_EnergyIntoR52to55rangeNis600/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoR52to55rangeNis800
-./mainCGEnergy2 5 729 572 52 55 0 bcsstk19.mtx >> CG_EnergyIntoR52to55rangeNis800/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoP52to55rangeNis200
-./mainCGEnergy2 5 729 144 52 55 1 bcsstk19.mtx >> CG_EnergyIntoP52to55rangeNis200/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoP52to55rangeNis400
-./mainCGEnergy2 5 729 288 52 55 1 bcsstk19.mtx >> CG_EnergyIntoP52to55rangeNis400/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoP52to55rangeNis600
-./mainCGEnergy2 5 729 450 52 55 1 bcsstk19.mtx >> CG_EnergyIntoP52to55rangeNis600/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoP52to55rangeNis800
-./mainCGEnergy2 5 729 572 52 55 1 bcsstk19.mtx >> CG_EnergyIntoP52to55rangeNis800/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoX52to55rangeNis200
-./mainCGEnergy2 5 729 144 52 55 2 bcsstk19.mtx >> CG_EnergyIntoX52to55rangeNis200/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoX52to55rangeNis400
-./mainCGEnergy2 5 729 288 52 55 2 bcsstk19.mtx >> CG_EnergyIntoX52to55rangeNis400/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoX52to55rangeNis600
-./mainCGEnergy2 5 729 450 52 55 2 bcsstk19.mtx >> CG_EnergyIntoX52to55rangeNis600/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoX52to55rangeNis800
-./mainCGEnergy2 5 729 572 52 55 2 bcsstk19.mtx >> CG_EnergyIntoX52to55rangeNis800/EnergyProp52to55result_bcsstk19.txt
-mkdir CG_EnergyIntoR48to51rangeNis200
-./mainCGEnergy2 5 729 144 48 51 0 bcsstk19.mtx >> CG_EnergyIntoR48to51rangeNis200/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoR48to51rangeNis400
-./mainCGEnergy2 5 729 288 48 51 0 bcsstk19.mtx >> CG_EnergyIntoR48to51rangeNis400/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoR48to51rangeNis600
-./mainCGEnergy2 5 729 450 48 51 0 bcsstk19.mtx >> CG_EnergyIntoR48to51rangeNis600/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoR48to51rangeNis800
-./mainCGEnergy2 5 729 572 48 51 0 bcsstk19.mtx >> CG_EnergyIntoR48to51rangeNis800/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoP48to51rangeNis200
-./mainCGEnergy2 5 729 144 48 51 1 bcsstk19.mtx >> CG_EnergyIntoP48to51rangeNis200/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoP48to51rangeNis400
-./mainCGEnergy2 5 729 288 48 51 1 bcsstk19.mtx >> CG_EnergyIntoP48to51rangeNis400/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoP48to51rangeNis600
-./mainCGEnergy2 5 729 450 48 51 1 bcsstk19.mtx >> CG_EnergyIntoP48to51rangeNis600/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoP48to51rangeNis800
-./mainCGEnergy2 5 729 572 48 51 1 bcsstk19.mtx >> CG_EnergyIntoP48to51rangeNis800/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoX48to51rangeNis200
-./mainCGEnergy2 5 729 144 48 51 2 bcsstk19.mtx >> CG_EnergyIntoX48to51rangeNis200/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoX48to51rangeNis400
-./mainCGEnergy2 5 729 288 48 51 2 bcsstk19.mtx >> CG_EnergyIntoX48to51rangeNis400/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoX48to51rangeNis600
-./mainCGEnergy2 5 729 450 48 51 2 bcsstk19.mtx >> CG_EnergyIntoX48to51rangeNis600/EnergyProp48to51result_bcsstk19.txt
-mkdir CG_EnergyIntoX48to51rangeNis800
-./mainCGEnergy2 5 729 572 48 51 2 bcsstk19.mtx >> CG_EnergyIntoX48to51rangeNis800/EnergyProp48to51result_bcsstk19.txt
+mkdir IntoR60to63rangeNis200
+./mainCGEnergy2 5 729 144 60 63 0 bcsstk19.mtx >> IntoR60to63rangeNis200/bcsstk19.txt
+mkdir IntoR60to63rangeNis400
+./mainCGEnergy2 5 729 288 60 63 0 bcsstk19.mtx >> IntoR60to63rangeNis400/bcsstk19.txt
+mkdir IntoR60to63rangeNis600
+./mainCGEnergy2 5 729 450 60 63 0 bcsstk19.mtx >> IntoR60to63rangeNis600/bcsstk19.txt
+mkdir IntoR60to63rangeNis800
+./mainCGEnergy2 5 729 572 60 63 0 bcsstk19.mtx >> IntoR60to63rangeNis800/bcsstk19.txt
+mkdir IntoP60to63rangeNis200
+./mainCGEnergy2 5 729 144 60 63 1 bcsstk19.mtx >> IntoP60to63rangeNis200/bcsstk19.txt
+mkdir IntoP60to63rangeNis400
+./mainCGEnergy2 5 729 288 60 63 1 bcsstk19.mtx >> IntoP60to63rangeNis400/bcsstk19.txt
+mkdir IntoP60to63rangeNis600
+./mainCGEnergy2 5 729 450 60 63 1 bcsstk19.mtx >> IntoP60to63rangeNis600/bcsstk19.txt
+mkdir IntoP60to63rangeNis800
+./mainCGEnergy2 5 729 572 60 63 1 bcsstk19.mtx >> IntoP60to63rangeNis800/bcsstk19.txt
+mkdir IntoX60to63rangeNis200
+./mainCGEnergy2 5 729 144 60 63 2 bcsstk19.mtx >> IntoX60to63rangeNis200/bcsstk19.txt
+mkdir IntoX60to63rangeNis400
+./mainCGEnergy2 5 729 288 60 63 2 bcsstk19.mtx >> IntoX60to63rangeNis400/bcsstk19.txt
+mkdir IntoX60to63rangeNis600
+./mainCGEnergy2 5 729 450 60 63 2 bcsstk19.mtx >> IntoX60to63rangeNis600/bcsstk19.txt
+mkdir IntoX60to63rangeNis800
+./mainCGEnergy2 5 729 572 60 63 2 bcsstk19.mtx >> IntoX60to63rangeNis800/bcsstk19.txt
+mkdir IntoR56to59rangeNis200
+./mainCGEnergy2 5 729 144 56 59 0 bcsstk19.mtx >> IntoR56to59rangeNis200/bcsstk19.txt
+mkdir IntoR56to59rangeNis400
+./mainCGEnergy2 5 729 288 56 59 0 bcsstk19.mtx >> IntoR56to59rangeNis400/bcsstk19.txt
+mkdir IntoR56to59rangeNis600
+./mainCGEnergy2 5 729 450 56 59 0 bcsstk19.mtx >> IntoR56to59rangeNis600/bcsstk19.txt
+mkdir IntoR56to59rangeNis800
+./mainCGEnergy2 5 729 572 56 59 0 bcsstk19.mtx >> IntoR56to59rangeNis800/bcsstk19.txt
+mkdir IntoP56to59rangeNis200
+./mainCGEnergy2 5 729 144 56 59 1 bcsstk19.mtx >> IntoP56to59rangeNis200/bcsstk19.txt
+mkdir IntoP56to59rangeNis400
+./mainCGEnergy2 5 729 288 56 59 1 bcsstk19.mtx >> IntoP56to59rangeNis400/bcsstk19.txt
+mkdir IntoP56to59rangeNis600
+./mainCGEnergy2 5 729 450 56 59 1 bcsstk19.mtx >> IntoP56to59rangeNis600/bcsstk19.txt
+mkdir IntoP56to59rangeNis800
+./mainCGEnergy2 5 729 572 56 59 1 bcsstk19.mtx >> IntoP56to59rangeNis800/bcsstk19.txt
+mkdir IntoX56to59rangeNis200
+./mainCGEnergy2 5 729 144 56 59 2 bcsstk19.mtx >> IntoX56to59rangeNis200/bcsstk19.txt
+mkdir IntoX56to59rangeNis400
+./mainCGEnergy2 5 729 288 56 59 2 bcsstk19.mtx >> IntoX56to59rangeNis400/bcsstk19.txt
+mkdir IntoX56to59rangeNis600
+./mainCGEnergy2 5 729 450 56 59 2 bcsstk19.mtx >> IntoX56to59rangeNis600/bcsstk19.txt
+mkdir IntoX56to59rangeNis800
+./mainCGEnergy2 5 729 572 56 59 2 bcsstk19.mtx >> IntoX56to59rangeNis800/bcsstk19.txt
+mkdir IntoR52to55rangeNis200
+./mainCGEnergy2 5 729 144 52 55 0 bcsstk19.mtx >> IntoR52to55rangeNis200/bcsstk19.txt
+mkdir IntoR52to55rangeNis400
+./mainCGEnergy2 5 729 288 52 55 0 bcsstk19.mtx >> IntoR52to55rangeNis400/bcsstk19.txt
+mkdir IntoR52to55rangeNis600
+./mainCGEnergy2 5 729 450 52 55 0 bcsstk19.mtx >> IntoR52to55rangeNis600/bcsstk19.txt
+mkdir IntoR52to55rangeNis800
+./mainCGEnergy2 5 729 572 52 55 0 bcsstk19.mtx >> IntoR52to55rangeNis800/bcsstk19.txt
+mkdir IntoP52to55rangeNis200
+./mainCGEnergy2 5 729 144 52 55 1 bcsstk19.mtx >> IntoP52to55rangeNis200/bcsstk19.txt
+mkdir IntoP52to55rangeNis400
+./mainCGEnergy2 5 729 288 52 55 1 bcsstk19.mtx >> IntoP52to55rangeNis400/bcsstk19.txt
+mkdir IntoP52to55rangeNis600
+./mainCGEnergy2 5 729 450 52 55 1 bcsstk19.mtx >> IntoP52to55rangeNis600/bcsstk19.txt
+mkdir IntoP52to55rangeNis800
+./mainCGEnergy2 5 729 572 52 55 1 bcsstk19.mtx >> IntoP52to55rangeNis800/bcsstk19.txt
+mkdir IntoX52to55rangeNis200
+./mainCGEnergy2 5 729 144 52 55 2 bcsstk19.mtx >> IntoX52to55rangeNis200/bcsstk19.txt
+mkdir IntoX52to55rangeNis400
+./mainCGEnergy2 5 729 288 52 55 2 bcsstk19.mtx >> IntoX52to55rangeNis400/bcsstk19.txt
+mkdir IntoX52to55rangeNis600
+./mainCGEnergy2 5 729 450 52 55 2 bcsstk19.mtx >> IntoX52to55rangeNis600/bcsstk19.txt
+mkdir IntoX52to55rangeNis800
+./mainCGEnergy2 5 729 572 52 55 2 bcsstk19.mtx >> IntoX52to55rangeNis800/bcsstk19.txt
+mkdir IntoR48to51rangeNis200
+./mainCGEnergy2 5 729 144 48 51 0 bcsstk19.mtx >> IntoR48to51rangeNis200/bcsstk19.txt
+mkdir IntoR48to51rangeNis400
+./mainCGEnergy2 5 729 288 48 51 0 bcsstk19.mtx >> IntoR48to51rangeNis400/bcsstk19.txt
+mkdir IntoR48to51rangeNis600
+./mainCGEnergy2 5 729 450 48 51 0 bcsstk19.mtx >> IntoR48to51rangeNis600/bcsstk19.txt
+mkdir IntoR48to51rangeNis800
+./mainCGEnergy2 5 729 572 48 51 0 bcsstk19.mtx >> IntoR48to51rangeNis800/bcsstk19.txt
+mkdir IntoP48to51rangeNis200
+./mainCGEnergy2 5 729 144 48 51 1 bcsstk19.mtx >> IntoP48to51rangeNis200/bcsstk19.txt
+mkdir IntoP48to51rangeNis400
+./mainCGEnergy2 5 729 288 48 51 1 bcsstk19.mtx >> IntoP48to51rangeNis400/bcsstk19.txt
+mkdir IntoP48to51rangeNis600
+./mainCGEnergy2 5 729 450 48 51 1 bcsstk19.mtx >> IntoP48to51rangeNis600/bcsstk19.txt
+mkdir IntoP48to51rangeNis800
+./mainCGEnergy2 5 729 572 48 51 1 bcsstk19.mtx >> IntoP48to51rangeNis800/bcsstk19.txt
+mkdir IntoX48to51rangeNis200
+./mainCGEnergy2 5 729 144 48 51 2 bcsstk19.mtx >> IntoX48to51rangeNis200/bcsstk19.txt
+mkdir IntoX48to51rangeNis400
+./mainCGEnergy2 5 729 288 48 51 2 bcsstk19.mtx >> IntoX48to51rangeNis400/bcsstk19.txt
+mkdir IntoX48to51rangeNis600
+./mainCGEnergy2 5 729 450 48 51 2 bcsstk19.mtx >> IntoX48to51rangeNis600/bcsstk19.txt
+mkdir IntoX48to51rangeNis800
+./mainCGEnergy2 5 729 572 48 51 2 bcsstk19.mtx >> IntoX48to51rangeNis800/bcsstk19.txt
  
